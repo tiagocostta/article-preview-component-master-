729d46acfb986a9055dfccb07feb8705b7965ad4
tiagocostta <tiagoct884@gmail.com>
2023-03-29 22:27:54 -0300

    first commit

---
+++ article-preview-component-master/index.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('#button_share')
+const share = document.querySelector(".share")
+
+button.addEventListener('click', () => {
+    share.classList.toggle('invisible')
+
+    
+});
